b77dd625d5217e9bb484c5db54ba7d1991acdc36
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-08-25 18:45:34 -0700

    added test array

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 /* setup your angular app here */
 
-//debug stuff to show the app is loading and fruit / veggies are available
+var myApp = angular.module('FruitAndVeggies', []);
+
+//myApp.config(function(){  });
+myApp.run(function(){
+  //debug stuff to show the app is loading and fruit / veggies are available
 console.log('App Started');
 console.log('Fruit count', fruit.length);
 console.log('Veggie count', vegetables.length);
+
+});
+
+myApp.controller('Separator', ['$scope', function($scope) {
+
+  $scope.fruitsAndVeggies = fruit.concat(vegetables);
+
+
+
+
+
+}]);
+
+
