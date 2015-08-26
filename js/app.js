6d2393964576e09cac756978581945971d638b89
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-08-25 18:50:49 -0700

    Added shuffle

--- js/app.js
+++ js/app.js
@@ -17,7 +17,13 @@ myApp.controller('Separator', ['$scope', function($scope) {
 
 
 
+function shuffle(o){
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+}
 
+//calling shuffle function
+shuffle($scope.fruitsAndVeggies);
 
 }]);
 
